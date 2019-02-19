c228d62e957ca29c8e0a19f535ffc03afdec8910
Mohammed Alotaibi <imohammed.mg@gmail.com>
2019-02-19 17:32:14 +0300

    task_14

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
-# Your imports should be here
+from rest_framework.generics import ListAPIView
+from .serializers import RestaurantListSerializer
+from restaurants.models import Restaurant
 
 
 # Complete me! I should be your APIListView
-class RestaurantListView():
-	...
+class RestaurantListView(ListAPIView):
+	queryset = Restaurant.objects.all()
+	serializer_class = RestaurantListSerializer
